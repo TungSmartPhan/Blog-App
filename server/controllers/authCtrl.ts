8912ae0181460956aa389593a9805b23b6c233f9
Tung Phan <tungptgcd18390@fpt.edu.vn>
2023-01-28 16:45:12 +0700

    Authentication Register Controller

---
+++ server/controllers/authCtrl.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import Users from "../models/userModel";
+import bcrypt from "bcrypt";
+import { Jwt } from "jsonwebtoken";
+
+const authCtrl = {
+  register: async (req: Request, res: Response) => {
+    try {
+      const { name, account, password } = req.body;
+      const user = await Users.findOne({ account });
+      if (!user)
+        return res
+          .status(400)
+          .json({ message: "Email or phone already registered" });
+
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      const newUser = new Users({
+        name,
+        account,
+        password: passwordHash,
+      });
+
+      res.json({ message: "Register Successfully", data: newUser });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: error });
+    }
+  },
+};
+
+export default authCtrl;
