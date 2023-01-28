694f28a45da7af35a1605966e4a582fbebd549ac
Tung Phan <tungptgcd18390@fpt.edu.vn>
2023-01-29 00:28:08 +0700

    set up middleware validation

---
+++ server/middleware/validation.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+
+export const validRegister = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, account, password } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: "Please add your name." });
+  } else if (name.length > 20) {
+    return res
+      .status(400)
+      .json({ message: "Your name is up too 20 chars long" });
+  }
+
+  if (!account) {
+    return res
+      .status(400)
+      .json({ message: "Please add your email or phone number." });
+  } else if (validPhone(account) && !validateEmail(account)) {
+    return res.status(400).json({ message: "Email or phone number format is incorrect" });
+  }
+
+  if(password) {
+    return res.status(400).json({ message: "Password must be at least 6 chars. "});
+  }
+};
+
+function validPhone(phone: string) {
+  const re = /^[+]/g;
+  return re.test(phone);
+}
+ 
+function validateEmail(email: string) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
