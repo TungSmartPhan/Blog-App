2f8334a4962ea54e81bfdce3ac74e892f3610460
Tung Phan <tungptgcd18390@fpt.edu.vn>
2023-01-29 00:45:39 +0700

    Add validation to router Register

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import authCtrl from "../controllers/authCtrl";
+import { validRegister } from "../middleware/validation";
 
 const router = express.Router();
 
-router.post('/register', authCtrl.register);
+router.post('/register', validRegister ,authCtrl.register);
 
 export default router;
